82945fcaa24f4f2c1adf03c452b51e55ca97dd9f
John Moosemiller <itcaughtonj@gmail.com>
2015-03-03 23:30:05 -0600

    Update imagefunction.m

--- imagefunction.m
+++ imagefunction.m
@@ -1,25 +1,32 @@
 function [] = imagefunction( filename, redval, greenval, blueval )
-%UNTITLED2 Summary of this function goes here
+%imagefunction Summary of this function goes here
 %   This function brightens the red values of the left 1/2 of the image (by
 %   redval), brightens green in the right 1/2 of the image (by greenval), 
 %   and darkens blue pixels (by blueval) where the blue values are greater 
 %   than 150 and then replaces the original file with the final image.
-image = imread(filename);
+image1 = imread(filename);
 
-[r,c] = size(image);
+[r,c,x] = size(image1);
 
-redlayer = image(:,:,1);
-greenlayer = image(:,:,2);
-bluelayer = image(:,:,3);
+redlayer = image1(:,:,1);
+greenlayer = image1(:,:,2);
+bluelayer = image1(:,:,3);
 
-redlayer(:,[1:(c/2)]) = redlayer(:,[1:(c/2)])+redval;
-greenlayer(:,(c/2+1):c) = greenlayer(:,(c/2+1):c)+greenval;
-bluemask = bluelayer > 150
+redlayer(:,[1:(c/2)]) = redlayer(:,[1:(c/2)])+redval;%This si where we brighten by our red val
 
-bluelayer(bluemask) = bluelayer(bluemask)-blueval
+greenlayer(:,[(c/2+1):c]) = greenlayer(:,[(c/2+1):c])+greenval;
 
-finalimage = cat(3,redlayer, greenlayer, bluelayer)
+bluemask = bluelayer > 150;
+bluelayer(bluemask) = bluelayer(bluemask)-blueval;
 
-imwrite(finalimage, "finalimage.png");
+finalimage = cat(3,redlayer, greenlayer, bluelayer);
 
+imwrite(finalimage,'notmundo.jpg');
+
+figure(1)
+image(finalimage)
+pause
+
+figure(2)
+image(image1)
 end
